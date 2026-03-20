da9af2d2754f4e75827d7eae33f4a8ba6bafa99f
Vishal Naidu <vminorr@gmail.com>
2026-03-20 21:58:54 +0530

    Setup face detection functionality using mediapipe

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { FaceDetector } from '@mediapipe/tasks-vision'
+import React from 'react'
+import FaceExpression from './features/expression/component/FaceExpression'
+
+const App = () => {
+  return (
+    <FaceExpression />
+  )
+}
+
+export default App
